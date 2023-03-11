a8bbc80cbd6037ed438bca40bc8e7668186e47ab
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-03-11 09:15:37 +0000

    Message de salutations une fois que l'utilisateur lance ce projet dans le Main.java

---
+++ src/main/java/maflore/tech/Main.java
@@ -0,0 +1,11 @@
+package maflore.tech;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello My Friends, moi c'est MaFlore!");
+        System.out.println("Je suis capable de vous aidez en générant le CRUD d'une entité en spring;");
+        System.out.println("Pour se faire, vous devez vous assurez que vous avez ajouter toutes");
+        System.out.println("les dependances possibles dans le pom.xml de votre projet générer depuis");
+        System.out.println("https://start.spring.io/");
+    }
+}
