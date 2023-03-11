c9ac766a2f3e88fa14caabf551a6716ccbbc5882
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-03-11 09:44:25 +0000

    Création de classe WriterFile pour créer un fichier et ecrire dans ce fichier

---
+++ src/main/java/packageAndFileGenerator/WriterFile.java
@@ -0,0 +1,20 @@
+package packageAndFileGenerator;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriterFile {
+
+    public void fileWriter(String cheminDuFichier, String contenu){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(cheminDuFichier));
+            writer.write(contenu);
+            writer.close();
+            System.out.println("Fichier écrit avec succès.");
+        } catch (IOException e) {
+            System.out.println("Erreur.");
+            e.printStackTrace();
+        }
+    }
+}
