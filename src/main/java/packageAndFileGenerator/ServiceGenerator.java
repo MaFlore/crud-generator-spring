27b70996839f92f4de4fb1d0f7761d3f85a27eec
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-03-15 01:40:24 +0000

    Revues et apports de modification aux commentaires

--- src/main/java/packageAndFileGenerator/ServiceGenerator.java
+++ src/main/java/packageAndFileGenerator/ServiceGenerator.java
@@ -9,15 +9,15 @@ public class ServiceGenerator {
                 "import " + nomDuPackage + ".model." + nomEntite + ";\n\n" +
                 "@Service\n" +
                 "public interface " + nomEntite + "Service {\n\n" +
-                "    /*Fonction retournant toutes les occurrences de l'entité*/\n\n" +
+                "    /*Fonction retournant toutes les occurrences de l'entité*/\n" +
                 "    List<" + nomEntite + "> findAll();\n\n" +
-                "    /*Fonction retournant les occurrences de l'entité en envoyant son id*/\n\n" +
+                "    /*Fonction retournant les occurrences de l'entité en envoyant son id*/\n" +
                 "    " + nomEntite + " findById(Long id);\n\n" +
-                "    /*Fonction d'ajout d'une occurence*/\n\n" +
+                "    /*Fonction d'ajout d'une occurence*/\n" +
                 "    " + nomEntite + " save("+ nomEntite.toLowerCase() + ");\n\n" +
-                "    /*Fonction de modification d'une occurence*/\n\n" +
+                "    /*Fonction de modification d'une occurence*/\n" +
                 "    " + nomEntite + "update("+ nomEntite.toLowerCase() + ");\n\n" +
-                "    /*Fonction de suppression d'une occurence par id*/\n\n" +
+                "    /*Fonction de suppression d'une occurence par id*/\n" +
                 "    void deleteById(Long id);\n\n" +
                 "}";
         WriterFile writerFile = new WriterFile();
